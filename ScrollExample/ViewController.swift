6520515998db4c27936cfc91b1416d212ee853ce
femialiu <femialiu713@gmail.com>
2021-10-21 16:49:22 +0100

    Fixed title label hugging safearea

--- ScrollExample/ViewController.swift
+++ ScrollExample/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = .systemBackground
+        
         setUpScrollView()
         setUpViews()
     }
@@ -41,7 +43,7 @@ class ViewController: UIViewController {
         label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         label.numberOfLines = 0
         label.sizeToFit()
-        label.textColor = UIColor.white
+        label.textColor = UIColor.blue
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -51,7 +53,7 @@ class ViewController: UIViewController {
         label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
         label.numberOfLines = 0
         label.sizeToFit()
-        label.textColor = UIColor.white
+        label.textColor = UIColor.blue
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -59,7 +61,7 @@ class ViewController: UIViewController {
     func setUpViews() {
         contentView.addSubview(titleLabel)
         titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 350).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
         
         contentView.addSubview(subtitleLabel)
