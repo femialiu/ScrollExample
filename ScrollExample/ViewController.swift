14c2f94bcd43c44e9d92928c728a74f267a94e55
femialiu <femialiu713@gmail.com>
2021-10-22 09:50:00 +0100

    added more content to increase content on scrollview

--- ScrollExample/ViewController.swift
+++ ScrollExample/ViewController.swift
@@ -58,17 +58,65 @@ class ViewController: UIViewController {
         return label
     }()
     
+    var otherTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var yetAnotherTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    var stillAnotherTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        label.numberOfLines = 0
+        label.sizeToFit()
+        label.textColor = UIColor.blue
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     func setUpViews() {
         contentView.addSubview(titleLabel)
         titleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 350).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
         titleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor).isActive = true
         
         contentView.addSubview(subtitleLabel)
         subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
         subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 25).isActive = true
         subtitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
-        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+//        subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        contentView.addSubview(otherTitleLabel)
+        otherTitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        otherTitleLabel.topAnchor.constraint(equalTo: subtitleLabel.bottomAnchor, constant: 25).isActive = true
+        otherTitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+//        otherTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        contentView.addSubview(yetAnotherTitleLabel)
+        yetAnotherTitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        yetAnotherTitleLabel.topAnchor.constraint(equalTo: otherTitleLabel.bottomAnchor, constant: 25).isActive = true
+        yetAnotherTitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+//        yetAnotherTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
+        
+        contentView.addSubview(stillAnotherTitleLabel)
+        stillAnotherTitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        stillAnotherTitleLabel.topAnchor.constraint(equalTo: yetAnotherTitleLabel.bottomAnchor, constant: 25).isActive = true
+        stillAnotherTitleLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 3/4).isActive = true
+        stillAnotherTitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor).isActive = true
     }
     
 }
